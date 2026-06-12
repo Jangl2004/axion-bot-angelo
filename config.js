0f5f8fb775380ab7c4edf5705d8c69364a6c6111
Jangl2004 <alessandromagno722@gmail.com>
2026-06-12 21:33:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -28,8 +28,9 @@ global.owner = [
   ['393780087063', 'bonzino²', true],
   ['447529461874', 'Elixir', true],
   ['2348174457298', 'Elixir', true],
-  ['393780306787', 'zak', true], 
+  ['393522890480', 'zak', true], 
   ['4915511934253', 'tom', true], 
+  
 ]
 
 global.mods = [
